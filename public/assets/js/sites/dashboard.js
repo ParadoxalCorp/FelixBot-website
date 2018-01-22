53c8ce2348fc0e9196410e3cab382bf2936ebe0e
ParadoxOrigins <funxgame1@gmail.com>
2018-01-22 07:22:36 +0100

    here, get a free working modal
    !!--This shouldn't be used in production-!!
    
    No but seriously i didn't tested all the changes, front-end is a pain in the ass, more like a cancer in fact. So as it seems the experience dashboard works, still need to see if all checkboxes works and do a lot of testing with how global variables react when changing the selected server

--- public/assets/js/sites/dashboard.js
+++ public/assets/js/sites/dashboard.js
@@ -206,35 +206,35 @@ $.get("/api/mutualGuilds", function (json) {
 						</div>
 						<div class="content">
 							<p>Below are your Felix-related data privacy settings</p>
-							<div class="ui toggle checkbox">
+							<div class="ui slider checkbox">
 								<input type="checkbox" name="upvote_privacy" id="upvotePrivacy">
 								<label>Public upvote</label>
 							</div>
 							<br>
 							<br>
 							<br>
-							<div class="ui toggle checkbox" id="pointsPrivacyContainer">
+							<div class="ui slider checkbox" id="pointsPrivacyContainer">
 								<input type="checkbox" name="points_privacy" id="pointsPrivacy">
 								<label>Public points</label>
 							</div>
 							<br>
 							<br>
 							<br>
-							<div class="ui toggle checkbox">
+							<div class="ui slider checkbox">
 								<input type="checkbox" name="love_privacy" id="lovePrivacy">
 								<label>Public love</label>
 							</div>
 							<br>
 							<br>
 							<br>
-							<div class="ui toggle checkbox">
+							<div class="ui slider checkbox">
 								<input type="checkbox" name="level_privacy" id="levelPrivacy">
 								<label>Public experience/level</label>
 							</div>
 							<br>
 							<br>
 							<br>
-							<div class="ui toggle checkbox">
+							<div class="ui slider checkbox">
 								<input type="checkbox" name="profile_privacy" id="profilePrivacy">
 								<label>Public profile</label>
 							</div>
@@ -492,7 +492,7 @@ $.get("/api/mutualGuilds", function (json) {
 				<div class="fields">
 					<div class="twelve wide field">		
 						<label>Minimum number of star per message</label>
-						<input type="text" id="txtNum" value=${selectedServer.database.starboard.minimum} >
+						<input type="text" class="txtNum" id="starboardMinimum" value=${selectedServer.database.starboard.minimum}>
 					</div>
 				</div>
 				<label>Targeted channel:</label>
@@ -566,7 +566,7 @@ $.get("/api/mutualGuilds", function (json) {
 	<img src=${selectedServer.icon === null ? "https://semantic-ui.com/images/wireframe/square-image.png" : `https://cdn.discordapp.com/icons/${selectedServer.id}/${selectedServer.icon}.png`}>
   </div>
   <div class="content">
-   <div class="ui toggle checkbox">
+   <div class="ui slider checkbox">
       <input type="checkbox" name="automatic_removal" id="automaticRemoval">
       <label>Automatic removal: Whether the user previous role should be removed whenever they reach a higher role</label>
    </div>
@@ -577,7 +577,7 @@ $.get("/api/mutualGuilds", function (json) {
 		<div class="fields">
 		  <div class="twelve wide field">
 			<label>Set custom level up message</label>
-			<textarea id="CustomLevelUpMsg" placeholder="Level up custom message here" ${selectedServer.database.levelSystem.enabled ? "" : "disabled"}></textarea>
+			<textarea id="CustomLevelUpMsg" placeholder="Level up custom message here" ${selectedServer.database.levelSystem.enabled ? "" : "disabled"}>${selectedServer.database.levelSystem.customNotif}</textarea>
 			<div class="sub header disabled"><i class="info circle icon"></i> Any instance of %USER%, %USERNAME%, %USERTAG% and %LEVEL% will respectively be replaced by the mention of the user, the username of the user, the username + discriminator of the user and the level they just reached</div>
 		  </div>
 		</div>
@@ -585,21 +585,22 @@ $.get("/api/mutualGuilds", function (json) {
 		<div class="ui fluid search selection dropdown LevelUpNotificationChannel">
 		  <i class="dropdown icon"></i>
 		  ${selectedServer.database.levelSystem.levelUpNotif ? '<div class="text">' + (selectedServer.database.levelSystem.levelUpNotif === "dm" 
-		  ? 'Direct Message' : '<div class="text">' + (selectedServer.channels.find((c) => c.id === selectedServer.database.levelSystem.levelUpNotif) 
-		  ? '#' + selectedServer.channels.find((c) => c.id === selectedServer.database.levelSystem.levelUpNotif).name : (selectedServer.database.levelSystem.levelUpNotif === "Channel" ? 'channel' : '#deleted-channel'))) + '</div>'
+		  ? 'Direct Message' : (selectedServer.channels.find((c) => c.id === selectedServer.database.levelSystem.levelUpNotif) 
+		  ? '#' + selectedServer.channels.find((c) => c.id === selectedServer.database.levelSystem.levelUpNotif).name : (selectedServer.database.levelSystem.levelUpNotif === "channel" ? 'channel' : (selectedServer.database.levelSystem.levelUpNotif ? '#deleted-channel' : 'Disabled')))) + '</div>'
 		  : '<div class="default text">Select channel</div>'}
 		  <div class="menu" id="LevelUpTargetsList">
 					  ${channelOptions(selectedServer)}
 					  <div class="item" data-value="dm">Direct Message</div>
 					  <div class="item" data-value="channel">Channel</div>
+					  <div class="item" data-value="disabled">Disabled</div>
 		  </div>
 		</div>
 		<br>
 	    <label>Assigned role(s) when users reach specific requirements:</label>
-		<div class="ui middle aligned divided list">
+		<div class="ui middle aligned divided list" id="activityRolesSet">
 		 ${selectedServer.database.levelSystem.roles.filter(r => selectedServer.roles.find(role => role.id === r.id)).map(r => `
-		 <div class="item" data-value="${r.id}">
-		  <div class="right floated content">
+		 <div class="item" data-value="${r.id}" amount-value="${r.at}" method-value="${r.method}">
+		  <div class="right floated content" onclick="this.parentNode.parentNode.removeChild(this.parentNode)">
 		   <div class="ui negative labeled icon button">Remove <i class="remove icon"></i></div>
 		  </div>
 		  <img class="ui avatar image" src="/imgs/${r.method === "experience" ? 'star' : 'mail'}-icon.png" style="border-radius: 0 !important"></img>
@@ -607,20 +608,36 @@ $.get("/api/mutualGuilds", function (json) {
 			 <a style="color: ${selectedServer.roles.find(role => role.id === r.id).hexColor}">${selectedServer.roles.find(role => role.id === r.id).name}</a> | At: ${r.method === "experience" ? 'Level ' + r.at : r.at + ' messages'}
 		  </div>
 		</div>`).join("")}
-		<div class="item">
-		<div class="right floated content">
-		 <div class="ui blue labeled icon button">Add <i class="plus icon"></i></div>
-		</div>
-		<div class="content">
+		<div class="ui card" id="newActivityRole">
+         <div class="content newActivityRoleCardContent">
+          <div class="header">Add new role</div>
+		  <div class="description">
 		   <div class="ui fluid search selection dropdown ActivityRoles">
-			  <i class="dropdown icon"></i>
-			  <div class="default text">Select role</div>
-			  <div class="menu" id="ActivityRolesTargetsList">
-				 ${roleOptions(selectedServer)}
-			  </div>
+		    <i class="dropdown icon"></i>
+		    <div class="default text">Select role</div>
+		    <div class="menu" id="ActivityRolesTargetsList">
+			  ${roleOptions(selectedServer)}
+		    </div>
 		   </div>
-		</div>
-	  </div>
+		   <br>
+		   <div class="ui fluid search selection dropdown ActivityCountersOptions">
+		    <i class="dropdown icon"></i>
+		    <div class="default text">Select counter</div>
+		    <div class="menu" id="ActivityCountersOptionsList">
+		     <div class="item" data-value="experience"><i class="star icon"></i> Level</div>
+		     <div class="item" data-value="message"><i class="mail icon"></i> Messages</div>
+		    </div>
+		  </div>
+		  <br>
+		  <div class="ui input">
+            <input type="text" id="newActivityRoleRequirement" class="txtNum" placeholder="Level or numbers of messages to get this role">
+          </div>
+         </div>
+        </div>
+       <div class="extra content">
+		 <div class="ui blue right floated labeled icon button" id="addNewActivityRoleRequirement">Add <i class="plus icon"></i></div>
+       </div>
+       </div>
 	</div>
 	</div>
   </div>
@@ -642,6 +659,9 @@ $.get("/api/mutualGuilds", function (json) {
 				`);
 			}
 		});
+		document.getElementById("automaticRemoval").addEventListener("click", () => {
+			data.levelSystemAutoRemoval = data.levelSystemAutoRemoval ? false : (typeof data.levelSystemAutoRemoval === "undefined" ? selectedServer.database.levelSystem.autoRemove : true);
+		});
 	});
 });
 
@@ -655,6 +675,8 @@ $(document).on("click", "#savePrivacySettingsButton", function () {
 	data.dataPrivacy = data.editedPrivacySettings;
 	postDataFunc(`/api/userData`, data);
 });
+
+
 
 // farewell settings
 
@@ -804,7 +826,7 @@ $(document).on("click", "#starboardModalSettings", function () {
 	$('.ui.dropdown.StarboardChannel').dropdown();
 });
 
-$(document).on("keypress", "#txtNum", function (e) {
+$(document).on("keypress", ".txtNum", function (e) {
 	var charCode = e.which || e.keyCode;
 	if (charCode < 48 || charCode > 57) {
 		return false;
@@ -817,7 +839,7 @@ $(document).on("keypress", "#txtNum", function (e) {
 $(document).on("click", "#saveStarboardSettingsButton", function () {
 	selectedServer.database.starboard.channel = document.getElementById('starboardTargetsList').getElementsByClassName('selected')[0] ?
 	document.getElementById('starboardTargetsList').getElementsByClassName('selected')[0].getAttribute('data-value') : false;
-	selectedServer.database.starboard.minimum = parseInt(document.getElementById('txtNum').value);
+	selectedServer.database.starboard.minimum = parseInt(document.getElementById('starboardMinimum').value);
 	postDataFunc(`/api/guildData`, selectedServer.database);
 });
 
@@ -859,20 +881,93 @@ $(document).on("click", "#levelSystemModalSettings", function () {
 		autofocus: false,
 	}).modal('show');
 
+	if (data.levelSystemAutoRemoval !== selectedServer.database.levelSystem.autoRemove) {
+		document.getElementById('automaticRemoval').click();
+	}
+
 	$('.ui.dropdown.LevelUpNotificationChannel').dropdown();
 	$('.ui.dropdown.ActivityRoles').dropdown();
+	$('.ui.dropdown.ActivityCountersOptions').dropdown();
+	for (let i = 0; i < document.getElementById("LevelUpTargetsList").children.length; i++) {
+		if (document.getElementById("LevelUpTargetsList").children[i].getAttribute('data-value')) {
+			if (document.getElementById("LevelUpTargetsList").children[i].getAttribute('data-value') === selectedServer.database.levelSystem.levelUpNotif || (document.getElementById("LevelUpTargetsList").children[i].getAttribute('data-value') === "disabled"  && !selectedServer.database.levelSystem.levelUpNotif)) {
+                document.getElementById("LevelUpTargetsList").children[i].classList.add("selected");
+			}
+		}
+	}
+});
+
+$(document).on("click", '#addNewActivityRoleRequirement', function() {
+	const newActivityRole = document.getElementById('newActivityRole'),
+		  targetRole = document.getElementById('ActivityRolesTargetsList').getElementsByClassName('selected')[0],
+		  counter = document.getElementById('ActivityCountersOptionsList').getElementsByClassName('selected')[0],
+		  requirement = document.getElementById("newActivityRoleRequirement").value;
+		  
+	if (!targetRole) return newActivityRole.getElementsByClassName('ActivityRoles')[0].classList.add('error');  
+	else newActivityRole.getElementsByClassName('ActivityRoles')[0].classList.remove('error');
+	if (!counter) return newActivityRole.getElementsByClassName('ActivityCountersOptions')[0].classList.add('error');
+	else newActivityRole.getElementsByClassName('ActivityCountersOptions')[0].classList.remove('error');
+	if (requirement === "") return document.getElementById("newActivityRoleRequirement").parentNode.classList.add('error');
+	else document.getElementById("newActivityRoleRequirement").parentNode.classList.remove('error');
+	
+	//Check if the role that is gonna be added is already set
+	for(let i = 0; i < document.getElementById("activityRolesSet").children.length; i++) {
+		if (document.getElementById("activityRolesSet").children[i].getAttribute('data-value') === targetRole.getAttribute('data-value')) {
+			if (!document.getElementById("alreadySetRoleErrorMessage")) return $('.content.newActivityRoleCardContent').append(`
+			   <div class="ui negative message" id="alreadySetRoleErrorMessage">
+			      <i class="close icon" onclick="this.parentNode.parentNode.removeChild(this.parentNode)"></i>
+				  <p>This role is already set to be given at some point</p>
+			  </div>
+			`); else return;
+		}
+	}
+	if (document.getElementById("alreadySetRoleErrorMessage")) {
+		document.getElementById("alreadySetRoleErrorMessage").parentNode.removeChild(document.getElementById("alreadySetRoleErrorMessage"));
+	}
+
+	$(`#activityRolesSet`).append(`
+	<div class="item" data-value="${targetRole.getAttribute('data-value')}" amount-value="${requirement}" method-value="${counter.getAttribute('data-value')}">
+	  <div class="right floated content" onclick="this.parentNode.parentNode.removeChild(this.parentNode)">
+		<div class="ui negative labeled icon button">Remove <i class="remove icon"></i></div>
+	  </div>
+	  <img class="ui avatar image" src="/imgs/${counter.getAttribute('data-value') === "experience" ? 'star' : 'mail'}-icon.png" style="border-radius: 0 !important"></img>
+	  <div class="content">
+		<a style="color: ${selectedServer.roles.find(role => role.id === targetRole.getAttribute('data-value')).hexColor}">${selectedServer.roles.find(role => role.id === targetRole.getAttribute('data-value')).name}</a> | At: ${counter.getAttribute('data-value') === "experience" ? 'Level ' + requirement : requirement + ' messages'}
+	  </div>
+   </div>
+  `)
+    document.getElementById('activityRolesSet').appendChild(document.getElementById('newActivityRole')) //fu jquery;
 });
 
 $(document).on("click", "#btnExperienceSystem", function () {
-	if ((document.getElementById("btnExperienceSystem").innerHTML) === "Disable") {
-		$('#CustomLevelUpMsg').prop("disabled", true);
-		document.getElementById("btnExperienceSystem").innerHTML = "Enable";
-		$('.ui.dropdown.GreetingChannel').addClass("disabled");
-		selectedServer.database.onEvent.guildMemberAdd.greetings.enabled = false;
-	} else {
+	if (!selectedServer.database.onEvent.guildMemberAdd.greetings.enabled) {
 		$('#CustomLevelUpMsg').prop("disabled", false);
 		document.getElementById("btnExperienceSystem").innerHTML = "Disable";
 		$('.ui.dropdown.GreetingChannel').removeClass("disabled");
 		selectedServer.database.onEvent.guildMemberAdd.greetings.enabled = true;
+	} else {
+		$('#CustomLevelUpMsg').prop("disabled", true);
+		document.getElementById("btnExperienceSystem").innerHTML = "Enable";
+		$('.ui.dropdown.GreetingChannel').addClass("disabled");
+		selectedServer.database.onEvent.guildMemberAdd.greetings.enabled = false;
 	}
+});
+
+$(document).on("click", "#saveExperienceSettingsButton", function () {
+	const activityRoles = [];
+	for (let i = 0; i < document.getElementById("activityRolesSet").children.length; i++) {
+		if (document.getElementById("activityRolesSet").children[i].getAttribute('data-value')) {
+			activityRoles.push({
+				id: document.getElementById("activityRolesSet").children[i].getAttribute('data-value'),
+				at: parseInt(document.getElementById("activityRolesSet").children[i].getAttribute('amount-value')),
+				method: document.getElementById("activityRolesSet").children[i].getAttribute('method-value')
+			});
+		}
+	}
+	selectedServer.database.levelSystem.roles = activityRoles;
+	selectedServer.database.levelSystem.enabled = document.getElementById("btnExperienceSystem").innerHTML === "Disable" ? true : false;
+	selectedServer.database.levelSystem.levelUpNotif = document.getElementById("LevelUpTargetsList").getElementsByClassName('selected')[0].getAttribute('data-value') === "disabled" ? false : document.getElementById("LevelUpTargetsList").getElementsByClassName('selected')[0].getAttribute('data-value');
+	selectedServer.database.levelSystem.customNotif = document.getElementById("CustomLevelUpMsg").value === "" ? false : document.getElementById("CustomLevelUpMsg").value;
+	selectedServer.database.levelSystem.autoRemove = data.levelSystemAutoRemoval;
+	postDataFunc(`/api/guildData`, selectedServer.database);
 });
