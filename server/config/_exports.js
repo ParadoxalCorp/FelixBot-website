0218331a15e7395998bc557de2224ccb50461a09
Niputi <7137178+Niputi@users.noreply.github.com>
2019-05-28 18:53:53 +0200

    clean state

--- server/config/_exports.js
+++
@@ -1,9 +0,0 @@
-"use strict";
-
-module.exports = {
-	_plugins: require('./pluginsConfig'),
-	_routes: require("./routeConfig"),
-	_views: require("./viewsConfig"),
-	_CookieAuth: require("./cookieAuthConfig"),
-	_BellAuth: require("./bellAuthConfig"),
-};
